f9caedce2c7bc9ad399127cb163d3592611d6f08
Ruben Ryckeghem <rubenryckeghem@macbook-pro-van-ruben.local>
2018-05-07 15:06:51 +0200

    Initial Commit

---
+++ Ruben_Ryckeghem_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ruben_Ryckeghem_S2IT_werkstuk1
+//
+//  Created by Ruben Ryckeghem on 7/05/18.
+//  Copyright © 2018 Ruben Ryckeghem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
